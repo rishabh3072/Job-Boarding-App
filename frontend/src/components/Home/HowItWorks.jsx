6faf67506d30737a8e181715ebeba80b350665e6
Rishabh Patidar <143130428+rishabh3072@users.noreply.github.com>
2025-02-14 00:03:52 +0530

    Update HowItWorks.jsx

--- frontend/src/components/Home/HowItWorks.jsx
+++ frontend/src/components/Home/HowItWorks.jsx
@@ -15,7 +15,7 @@ const HowItWorks = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "https://job-boarding-app-frontend.onrender.com/api/v1/user/logout",
         {
           withCredentials: true,
         }
