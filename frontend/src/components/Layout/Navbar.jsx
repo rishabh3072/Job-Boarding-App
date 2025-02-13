12fc39fc4e6aea1c5158597c17e63a39611cbfac
Rishabh Patidar <143130428+rishabh3072@users.noreply.github.com>
2025-02-14 00:06:55 +0530

    Update Navbar.jsx

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "https://job-boarding-app-frontend.onrender.comapi/v1/user/logout",
         {
           withCredentials: true,
         }
