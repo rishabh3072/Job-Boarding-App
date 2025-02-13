0cca2636f6ae29e08481ca20a37ad6b40dffbb2e
Rishabh Patidar <143130428+rishabh3072@users.noreply.github.com>
2025-02-14 01:27:09 +0530

    Update profileService.jsx

--- frontend/src/services/profileService.jsx
+++ frontend/src/services/profileService.jsx
@@ -3,27 +3,27 @@ import axios from 'axios';
 
 
 export const getEmployerProfile = async () => {
-  const response = await axios.get(`https://job-boarding-app-frontend.onrender.com/api/v1/employer/profile`);
+  const response = await axios.get(`https://job-boarding-app-backend.onrender.com/api/v1/employer/profile`);
   return response.data;
 };
 
 export const updateEmployerProfile = async (profileData) => {
-  const response = await axios.put(`https://job-boarding-app-frontend.onrender.com/api/v1/employer/profile`, profileData);
+  const response = await axios.put(`https://job-boarding-app-backend.onrender.com/api/v1/employer/profile`, profileData);
   return response.data;
 };
 
 export const deleteEmployerProfile = async () => {
-  const response = await axios.delete(`https://job-boarding-app-frontend.onrender.com/api/v1/employer/profile`);
+  const response = await axios.delete(`https://job-boarding-app-backend.onrender.com/api/v1/employer/profile`);
   return response.data;
 };
 
 export const getJobSeekerProfile = async () => {
-  const response = await axios.get(`https://job-boarding-app-frontend.onrender.com/api/v1/jobseeker/profile`);
+  const response = await axios.get(`https://job-boarding-app-backend.onrender.com/api/v1/jobseeker/profile`);
   return response.data;
 };
 
 export const updateJobSeekerProfile = async (profileData) => {
-  const response = await axios.put(`https://job-boarding-app-frontend.onrender.com/api/v1/jobseeker/profile`, profileData);
+  const response = await axios.put(`https://job-boarding-app-backend.onrender.com/api/v1/jobseeker/profile`, profileData);
   return response.data;
 };
 
